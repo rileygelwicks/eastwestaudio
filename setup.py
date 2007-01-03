d83e298d8fe7104069e27f4ec906a5f04477c22f
smulloni <smulloni@localhost>
2007-01-03 03:40:18 +0000

    dist, doc tweaks

--- setup.py
+++ setup.py
@@ -8,12 +8,18 @@ sys.path.insert(0, 'src')
 from ewa import __version__ as version
 
 description="an mp3 splicing system"
+long_description="""
+EWA (East-West Audio) is a server application that can
+dynamically add intros and outros to mp3s based on user-defined
+rules, so that podcasters can rotate promotional material included
+in their mp3 downloads without remastering.
+"""
 platforms="OS Independent"
 
 keywords=["mp3", "audio"]
 classifiers=filter(None, """
 
-Development Status :: 3 - Alpha
+Development Status :: 4 - Beta
 Intended Audience :: System Administrators
 Intended Audience :: Developers
 Operating System :: OS Independent
@@ -25,7 +31,9 @@ Topic :: Software Development :: Libraries :: Python Modules
 
 setup(author='Jacob Smullyan',
       author_email='jsmullyan@gmail.com',
+      url='http://eastwestaudio.wnyc.org/',
       description=description,
+      long_description=long_description,
       keywords=keywords,
       platforms=platforms,
       license='GPL',
@@ -34,6 +42,7 @@ setup(author='Jacob Smullyan',
       zip_safe=True,
       packages=['ewa'],
       package_dir={'' : 'src'},
+      test_suite='nose.collector',
       ext_modules=[Extension('ewa.frameinfo',
                              ['src/ewa/frameinfo.c'])],
       scripts=['bin/ewasplice',
